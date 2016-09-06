d4698195f111772e1ef804c19ad88811bcc38bb0
Blake Embrey <hello@blakeembrey.com>
2016-09-06 12:09:27 +1000

    Add test for CNN homepage

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -109,6 +109,7 @@ var FIXTURES = [
   'http://www.reddit.com/',
   'http://ogp.me/',
   'https://techcrunch.com/',
+  'http://cnn.com/',
   // Blog/article posts.
   'http://www.lifehacker.com.au/2016/08/some-bill-providers-automatically-update-your-credit-card-when-you-get-a-new-one/',
   'http://www.theverge.com/2016/8/10/12416766/google-white-house-ostp-emails?utm_campaign=theverge&utm_content=chorus&utm_medium=social&utm_source=twitter',
